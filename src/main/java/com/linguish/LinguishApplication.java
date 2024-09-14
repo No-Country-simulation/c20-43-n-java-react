125c7f18fe2857a468b0184e908b184ee90e08d7
Cris Arévalo <73801509+CrisArevalo@users.noreply.github.com>
2024-09-13 23:37:39 -0300

    Update LinguishApplication.java for deploy

--- src/main/java/com/linguish/LinguishApplication.java
+++ src/main/java/com/linguish/LinguishApplication.java
@@ -2,12 +2,16 @@ package com.linguish;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class LinguishApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.	run(LinguishApplication.class, args);
+		SpringApplication.run(LinguishApplication.class, args);
 	}
 
 	@Configuration
