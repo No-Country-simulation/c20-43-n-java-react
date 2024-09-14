8dbaea0f282a699f34834b7a39b36d84af44faf5
Cris Arévalo <73801509+CrisArevalo@users.noreply.github.com>
2024-09-13 23:29:06 -0300

    Update LinguishApplication.java for deploy

--- src/main/java/com/linguish/LinguishApplication.java
+++ src/main/java/com/linguish/LinguishApplication.java
@@ -10,4 +10,18 @@ public class LinguishApplication {
 		SpringApplication.	run(LinguishApplication.class, args);
 	}
 
+	@Configuration
+	public static class Myconfiguration{
+		@Bean
+		public WebMvcConfigurer corsConfigurer(){
+			return new WebMvcConfigurer() {
+				@Override
+				public void addCorsMappings(CorsRegistry registry) {
+					registry.addMapping("/**")
+							.allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
+				}
+			};
+		}
+	}
+
 }
